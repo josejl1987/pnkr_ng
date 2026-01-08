aca5f1cc680bdbf94f2c7a1c86baaeba36333ca8
Jose <josejl1987@gmail.com>
2026-01-08 22:43:04 +0100

    Add BoundingBox intersection helper

--- engine/include/pnkr/renderer/scene/Bounds.hpp
+++ engine/include/pnkr/renderer/scene/Bounds.hpp
@@ -28,6 +28,13 @@ namespace pnkr::renderer::scene
         {
             return m_min.x <= m_max.x && m_min.y <= m_max.y && m_min.z <= m_max.z;
         }
+
+        bool intersects(const BoundingBox& other) const
+        {
+            return (m_min.x <= other.m_max.x && m_max.x >= other.m_min.x) &&
+                   (m_min.y <= other.m_max.y && m_max.y >= other.m_min.y) &&
+                   (m_min.z <= other.m_max.z && m_max.z >= other.m_min.z);
+        }
     };
 
     class SceneGraphDOD;
